b5da9f9957c59a9826645a64e423339a5cff74d4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-31 13:52:41 +0430

    Optimize layer-norm

--- src/funcs/layer_norm.rs
+++ src/funcs/layer_norm.rs
@@ -18,13 +18,15 @@ const EPSILON: f32 = 1e-5;
 impl Function for LayerNorm {
     fn run(&mut self, inps: &[&Tensor<f32>], _training: bool) -> Tensor<f32> {
         self.norm = inps[0].map(1, |l| {
-            let avg = l.blob().iter().sum::<f32>() / l.size() as f32;
-            let var = (l.blob().iter().map(|f| (f - avg).powi(2)).sum::<f32>() / l.size() as f32
+            let size_inv = 1. / l.size() as f32;
+            let avg = l.blob().iter().sum::<f32>() * size_inv;
+            let var = (l.blob().iter().map(|f| (f - avg).powi(2)).sum::<f32>() * size_inv
                 + EPSILON)
                 .sqrt();
+            let var_inv = 1. / var;
             Tensor::raw(
                 l.shape(),
-                l.blob().iter().map(|v| (v - avg) / var).collect(),
+                l.blob().iter().map(|v| (v - avg) * var_inv).collect(),
             )
         });
         &(&self.norm * inps[1]) + inps[2]
@@ -38,9 +40,12 @@ impl Function for LayerNorm {
         let jacobian = inps[0].map(1, |l| {
             let n = l.size();
             let nf = n as f32;
+            let nf_inv = 1. / nf;
             let avg = l.blob().iter().sum::<f32>() / nf;
             let sigma2 = l.blob().iter().map(|f| (f - avg).powi(2)).sum::<f32>() / nf + EPSILON;
+            let sigma2_inv = 1. / sigma2;
             let sigma = sigma2.sqrt();
+            let sigma_inv = 1. / sigma;
             Tensor::raw(
                 &[n, n],
                 (0..n * n)
@@ -50,11 +55,13 @@ impl Function for LayerNorm {
                         let j = work % n;
                         if i == j {
                             let a = l.blob()[i];
-                            ((1. - 1. / nf) * sigma - (a - avg).powi(2) / sigma / nf) / sigma2
+                            ((1. - nf_inv) * sigma - (a - avg).powi(2) * sigma_inv * nf_inv)
+                                * sigma2_inv
                         } else {
                             let a = l.blob()[i];
                             let b = l.blob()[j];
-                            (-1. / nf * sigma - (b - avg) * (a - avg) / sigma / nf) / sigma2
+                            (-1. / nf * sigma - (b - avg) * (a - avg) * sigma_inv * nf_inv)
+                                * sigma2_inv
                         }
                     })
                     .collect(),

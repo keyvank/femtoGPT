d7f43818d8770845b81d5cdb923b095ed4734620
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-06-09 13:27:14 +0430

    Fix mask gradient! :facepalm:

--- src/funcs/mask.rs
+++ src/funcs/mask.rs
@@ -3,12 +3,14 @@ use super::{Function, Tensor, TensorOps};
 #[derive(Debug, Clone)]
 pub struct Mask {
     mask: Tensor<f32>,
+    mask_rev: Tensor<f32>,
     value: f32,
 }
 impl Mask {
     pub fn new(mask: Tensor<bool>, value: f32) -> Box<dyn Function> {
         Box::new(Self {
             mask: (&mask).into(),
+            mask_rev: (&!&mask).into(),
             value,
         })
     }
@@ -27,7 +29,7 @@ impl Function for Mask {
         })
     }
     fn grad(&self, _inps: &[&Tensor<f32>], out_grad: &Tensor<f32>) -> Vec<Tensor<f32>> {
-        vec![out_grad * &self.mask]
+        vec![out_grad * &self.mask_rev]
     }
     fn clone_box(&self) -> Box<dyn Function> {
         Box::new(self.clone())

b57599cb265588a3edec706d08e86a265477f1d0
baehyunsol <baehyunsol@naver.com>
2025-07-11 23:03:50 +0900

    fix signature of `ByteTokenizer::new()`
    I just copy-pasted the signature of `SimpleTokenizer::new()` and forgot
    to remove the argument. I removed it.

--- src/tokenizer/byte.rs
+++ src/tokenizer/byte.rs
@@ -4,7 +4,7 @@ use std::collections::{HashMap, HashSet};
 pub struct ByteTokenizer;
 
 impl ByteTokenizer {
-    pub fn new(_: &str) -> Self {
+    pub fn new() -> Self {
         ByteTokenizer
     }
 }
